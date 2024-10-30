4e383e24c1c815ea9ad576388a00fa313f910f7a
Mayara Arantes <mayaraarantes9@gmail.com>
2024-10-30 20:08:08 -0300

    IF e ELSE

---
+++ IF e else/script.js
@@ -0,0 +1,59 @@
+console.log("Hello World!!")
+
+let mesAtual = "Outubro" // Strings - Dados Textuais
+
+mesAtual = "Novembro"
+
+const mesesDoAno = 12 // Number - Dado Numerico
+
+let primeiroNome = "João"
+console.log(primeiroNome)
+
+let inverno = false // Boolean - True ou False - Verdadeiro ou Falso
+
+
+// condição simples
+// if(condicao){
+
+// }
+
+
+let botaoAtivo = true;
+
+if (botaoAtivo) {
+  console.log("Seu background ficou preto")
+}
+
+// condicao composta
+
+let maiorDeIdade = false;
+
+if (maiorDeIdade) {
+  console.log("Você pode se cadastrar no facebook")
+}
+else {
+  console.log("Você é menor de idade, então não pode ser cadastrar no facebook")
+}
+
+
+let palpiteDaSorte = 51;
+
+if (palpiteDaSorte === 3) {
+  console.log("Parabéns!!! voce acabou de ganhar 2 milhoes")
+}
+else {
+  console.log("Que pena! voce errou! :(")
+}
+
+
+let idade = 61;
+
+if (idade < 12) {
+  console.log(" CRIANÇA");
+} else if (idade <= 18) {
+  console.log(" ADOLESCENTE");
+} else if (idade <= 60) {
+  console.log("ADULTO");
+} else {
+  console.log("IDOSO");
+}
